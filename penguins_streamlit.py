a092895de0ee21dc7dec46a2aa8a7e36930f82f7
Lamine Mbaye <laminfo0093@gmail.com>
2025-11-04 15:35:37 +0000

    Password added

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -4,10 +4,18 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 st.title("Penguins Classifier :  A Machine Learning App")
 st.write("This app uses 6 inputs to predict the species of penguin using" 
          "a model built on the Palmer Penguins dataset. Use the form below" " to get started!")
+
+password_guess = st.text_input('What is the Password?')
+if password_guess != 'streamlit_password':
+    st.stop()
+penguin_file = st.file_uploader('Upload your own penguin data')
 
 penguin_df = pd.read_csv('penguins.csv')
 
