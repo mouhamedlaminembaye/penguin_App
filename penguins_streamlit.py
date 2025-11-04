18ab0fc085f91dbf8ddb487a4e3900672867915c
Lamine Mbaye <laminfo0093@gmail.com>
2025-11-04 15:40:41 +0000

    Cance password

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -12,10 +12,10 @@ st.title("Penguins Classifier :  A Machine Learning App")
 st.write("This app uses 6 inputs to predict the species of penguin using" 
          "a model built on the Palmer Penguins dataset. Use the form below" " to get started!")
 
-password_guess = st.text_input('What is the Password?')
-if password_guess != 'streamlit_password':
-    st.stop()
-penguin_file = st.file_uploader('Upload your own penguin data')
+# password_guess = st.text_input('What is the Password?')
+# if password_guess != 'streamlit_password':
+#     st.stop()
+# penguin_file = st.file_uploader('Upload your own penguin data')
 
 penguin_df = pd.read_csv('penguins.csv')
 
